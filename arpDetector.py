97d545c7a81ec0a3658b4bf52b234ce79df17e8a
Stefan Merkel <merkel.s.ulm@gmail.com>
2017-01-01 17:37:45 +0100

    create arpDetector

---
+++ arpDetector.py
@@ -0,0 +1,27 @@
+from eth import *
+from arp import *
+
+def PrintFrame(frameAsHexArray):
+    print('[*] Frame:')
+    print('[+]                                 ' + str(frameAsHexArray))
+    print('')
+
+def PrintEth(ethAsHexArray):
+    print('[*] Ethernet:')
+    print('[+]     Destination MAC address:    ' + str(GetEthDestinationMacAddressAsHexArray(ethAsHexArray)))
+    print('[+]     Source MAC address:         ' + str(GetEthSourceMacAddressAsHexArray(ethAsHexArray)))
+    print('[+]     Type:                       ' + str(GetEthTypeAsHexArray(ethAsHexArray)))
+    print('')
+
+def PrintArp(arpAsHexArray):
+    print('[*] Address Resolution Protocol:')
+    print('[+]     Hardware type:              ' + str(GetArpHardwareTypeAsHexArray(arpAsHexArray)))
+    print('[+]     Protocol type:              ' + str(GetArpProtocolTypeAsHexArray(arpAsHexArray)))
+    print('[+]     Hardware size:              ' + str(GetArpHardwareSizeAsHexArray(arpAsHexArray)))
+    print('[+]     Protocol size:              ' + str(GetArpProtocolSizeAsHexArray(arpAsHexArray)))
+    print('[+]     Opcode:                     ' + str(GetArpOpcodeAsHexArray(arpAsHexArray)))
+    print('[+]     Sender Mac Address:         ' + str(GetArpSenderMacAddressAsHexArray(arpAsHexArray)))
+    print('[+]     Sender Ip Address:          ' + str(GetArpSenderIpAddressAsHexArray(arpAsHexArray)))
+    print('[+]     Target Mac Address:         ' + str(GetArpTargetMacAddressAsHexArray(arpAsHexArray)))
+    print('[+]     Target Ip Address:          ' + str(GetArpTargetIpAddressAsHexArray(arpAsHexArray)))
+    print('')
